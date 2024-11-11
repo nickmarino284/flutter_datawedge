36e334cf563afe80c3555d34fab35b66a7cec776
Nick (Shark) Marino <nick.marino@beeso.io>
2024-11-11 15:41:49 -0600

    Update FlutterDatawedgePlugin.kt

--- android/src/main/kotlin/com/circuskitchens/flutter_datawedge/FlutterDatawedgePlugin.kt
+++ android/src/main/kotlin/com/circuskitchens/flutter_datawedge/FlutterDatawedgePlugin.kt
@@ -1,14 +1,11 @@
 package com.circuskitchens.flutter_datawedge
 
-
-
 import android.content.Context
 import android.content.IntentFilter
 import android.os.Build.*
 import android.os.Build.VERSION_CODES.*
 import android.util.Log
 import androidx.annotation.NonNull
-import androidx.annotation.RequiresApi
 import com.circuskitchens.flutter_datawedge.pigeon.DataWedgeFlutterApi
 import com.circuskitchens.flutter_datawedge.pigeon.DataWedgeHostApi
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -25,20 +22,18 @@ class FlutterDatawedgePlugin : FlutterPlugin {
     private var dwInterface: DWInterface? = null
     private lateinit var intentFilter: IntentFilter
 
-    @RequiresApi(LOLLIPOP)
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        Log.d("FlutterDataWedgeFlugin","Attaching to engine...")
+        Log.d("FlutterDataWedgePlugin", "Attaching to engine...")
         val api = DataWedgeFlutterApi(flutterPluginBinding.binaryMessenger)
-        dwInterface = DWInterface(flutterPluginBinding.applicationContext,api)
+        dwInterface = DWInterface(flutterPluginBinding.applicationContext, api)
         flutterApi = api
-        DataWedgeHostApi.setUp(flutterPluginBinding.binaryMessenger,dwInterface)
+        DataWedgeHostApi.setUp(flutterPluginBinding.binaryMessenger, dwInterface)
         dwInterface?.setupBroadcastReceiver()
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         dwInterface?.dispose()
-        DataWedgeHostApi.setUp(binding.binaryMessenger,null)
+        DataWedgeHostApi.setUp(binding.binaryMessenger, null)
         flutterApi = null
     }
-
 }
